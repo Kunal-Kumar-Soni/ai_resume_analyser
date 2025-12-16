1aa3cdd0d6277fbef8026fe9609d69b411c5ef5a
Kunal <kunal.codes24@gmail.com>
2025-12-16 20:43:07 +0530

    setup supabase

---
+++ context/AuthProvider.tsx
@@ -0,0 +1,39 @@
+import { createContext, useEffect, useState } from "react";
+import { supabaseClient } from "../lib/supabaseClient";
+import type { User } from "@supabase/supabase-js";
+
+type AuthContextType = {
+  user: User | null;
+  isLoading: boolean;
+};
+
+export const AuthContext = createContext<AuthContextType | null>(null);
+
+type AuthProviderProps = {
+  children: React.ReactNode;
+};
+
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  const getData = async () => {
+    setIsLoading(true);
+    const { data } = await supabaseClient.auth.getSession();
+    setUser(data.session?.user ?? null);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getData();
+    const { data: listener } = supabaseClient.auth.onAuthStateChange((e, session) => {
+      setUser(session?.user ?? null);
+    });
+
+    return () => {
+      listener.subscription.unsubscribe();
+    };
+  }, []);
+
+  return <AuthContext.Provider value={{ user, isLoading }}>{children}</AuthContext.Provider>;
+};
