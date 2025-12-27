f3c07e20428e1c00d9debf56be9303518fa8b16c
Kunal <kunal.codes24@gmail.com>
2025-12-27 18:27:23 +0530

    auth file update

--- app/auth/callback/page.tsx
+++ app/auth/callback/page.tsx
@@ -2,33 +2,13 @@
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { supabaseClient } from "@/lib/supabaseClient";
 import PageLoader from "@/components/ui/custom-animated-loader";
 
 export default function AuthCallback() {
   const router = useRouter();
 
   useEffect(() => {
-    const handleAuthCallback = async () => {
-      try {
-        const { data, error } = await supabaseClient.auth.getSession();
-        if (error) {
-          console.error("Error getting session:", error);
-          router.push("/signin");
-          return;
-        }
-        if (data.session) {
-          router.push("/");
-        } else {
-          router.push("/signin");
-        }
-      } catch (err) {
-        console.error("Unexpected error:", err);
-        router.push("/signin");
-      }
-    };
-
-    handleAuthCallback();
+    router.push("/");
   }, [router]);
 
   return <PageLoader />;
