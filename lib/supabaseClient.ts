de25cd6c65977b09bf73623a84798cb26e30587c
Kunal <kunal.codes24@gmail.com>
2025-12-27 19:43:17 +0530

    supabase client update

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -3,4 +3,11 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
-export const supabaseClient = createClient(supabaseUrl, supabaseAnonKey);
+export const supabaseClient = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true,
+    flowType: "implicit",
+  },
+});
