b12280e67aac25808cac15c45a6508ddaa2c1a02
Kunal <kunal.codes24@gmail.com>
2025-12-27 18:22:46 +0530

    auth file add

--- components/signin-form.tsx
+++ components/signin-form.tsx
@@ -89,7 +89,7 @@ export function SigninForm({ className, ...props }: React.ComponentProps<"div">)
     const { error } = await supabaseClient.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${window.location.origin}`,
+        redirectTo: `${window.location.origin}/auth/callback`,
         queryParams: {
           prompt: "select_account",
         },
